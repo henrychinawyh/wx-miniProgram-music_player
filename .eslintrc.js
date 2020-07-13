ba6501ed4d086d1da7097c348e89b1a0f6065391
王宇航-hz19114716 <hz19114716@huize.com>
2020-07-13 18:56:04 +0800

    initital program

---
+++ .eslintrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  extends: ["taro/react"],
+  "no-unused-vars": "none",
+};
---
+++ src/pages/index/index.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { View, Button, Text } from "@tarojs/components";
+
+import { add, minus, asyncAdd } from "../../actions/counter";
+
+import "./index.scss";
+
+@connect(
+  ({ counter }) => ({
+    counter,
+  }),
+  (dispatch) => ({
+    add() {
+      dispatch(add());
+    },
+    dec() {
+      dispatch(minus());
+    },
+    asyncAdd() {
+      dispatch(asyncAdd());
+    },
+  })
+)
+class Index extends Component {
+  componentWillUnmount() {}
+
+  componentDidShow() {}
+
+  componentDidHide() {}
+
+  render() {
+    return <View className="index"></View>;
+  }
+}
+
+export default Index;
